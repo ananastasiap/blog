5faa26774defa8aa326b5238573daebeb53814f3
ananastasiap <anestheziia@gmail.com>
2023-11-01 23:28:07 +0300

    feat: buy-me-coffee functionality

--- src/components/buy-me-coffee/buy-me-coffee.jsx
+++ src/components/buy-me-coffee/buy-me-coffee.jsx
@@ -3,11 +3,24 @@ import React from "react";
 import cl from 'classnames';
 import styles from './buy-me-coffee.module.scss';
 
+import ScreenEgg from "../screen-egg/screen-egg";
+
 const BuyMeCoffee = ({
   className,
 }) => {
   return(
-    <div>BuyMeCoffee</div>
+    <ScreenEgg type="right">
+    <div className={cl(className, styles.buyCoffee)}>
+      <a
+        href="https://buy.stripe.com/test_8wM8y1dOV1Uj0fK000"
+        target="_blank"
+        className={styles.buyCoffeeButton}
+        rel="noreferrer"
+      >
+        Buy this writer a coffe :)
+      </a>
+    </div>
+    </ScreenEgg>
   )
 }
 
